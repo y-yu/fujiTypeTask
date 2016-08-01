8266911388e450eb088fa5dbd5123ad908da40aa
Yoshimura Yuu <m@yyu.pw>
2016-08-01 11:12:56 +0900

    fix

--- src/main/scala/SubResource.scala
+++ src/main/scala/SubResource.scala
@@ -1,8 +1,22 @@
+trait <-<[A, B]
+
+trait <*<[A, B]
+
 trait :+:[A, B]
 
 trait :->[A <: (_ :+: _), B]
 
 trait :*>[A, B]
+
+object <*< {
+  implicit def self[A]: A <*< A = new (A <*< A) {}
+
+  implicit def transitive[A, B, C](implicit A: A <-< B, B: B <*< C): A <*< C = new (A <*< C) {}
+}
+
+object :-> {
+  implicit def order[A, B, C](implicit A: C <*< A, B: C <*< B): ((A :+: B) :-> C) = new ((A :+: B) :-> C) {}
+}
 
 object :*> {
   implicit def native[A, B, C](implicit R: (A :+: B) :-> C): ((A :+: B) :*> C) = new ((A :+: B) :*> C) {}
--- src/main/scala/Transaction.scala
+++ src/main/scala/Transaction.scala
@@ -6,11 +6,20 @@ case class ReadWriteTransaction()
 
 case class ReadBatchTransaction()
 
+case class ReadWriteBatchTransaction()
+
 object Implicits {
-  implicit val read = new ((ReadTransaction :+: Transaction) :-> ReadTransaction) {}
+  //implicit val read = new ((ReadTransaction :+: Transaction) :-> ReadTransaction) {}
+  //implicit val readwrite = new ((ReadWriteTransaction :+: ReadTransaction) :-> ReadWriteTransaction) {}
+  //implicit val readBatch = new ((ReadBatchTransaction :+: ReadTransaction) :-> ReadBatchTransaction) {}
+  implicit val read = new (ReadTransaction <-< Transaction) {}
 
-  implicit val readwrite = new ((ReadWriteTransaction :+: ReadTransaction) :-> ReadWriteTransaction) {}
+  implicit val readWrite = new (ReadWriteTransaction <-< ReadTransaction) {}
 
-  implicit val readBatch = new ((ReadBatchTransaction :+: ReadTransaction) :-> ReadBatchTransaction) {}
+  implicit val readBatch = new (ReadBatchTransaction <-< ReadTransaction) {}
+
+  implicit val readWriteBatch1 = new (ReadWriteBatchTransaction <-< ReadWriteTransaction) {}
+
+  implicit val readWriteBatch2 = new (ReadWriteBatchTransaction <-< ReadBatchTransaction) {}
 }
 
